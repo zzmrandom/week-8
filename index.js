5bcf270dd8fa67609931acf1582d6a29f5911e04
zzmrandom <149000037+zzmrandom@users.noreply.github.com>
2023-11-20 22:28:49 +0000

    editing js
    testing js code

--- index.js
+++ index.js
@@ -67,8 +67,8 @@ function displayForecast() {
     forecastHtml =
       forecastHtml +
       `
-<div class="weather-forecast">
-<div class="row">
+<div class="weather-forecast"></div>
+<div class="row"></div>
           <div class="col-2 day-column">
             <div class="day">${day}</div>
             <div class="weather-icon">
